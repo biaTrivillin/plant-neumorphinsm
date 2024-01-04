7417adaa5f4edc3ad3b12cf76004a4e0b06baadf
BiaTrivillin <biatrivillin.code@gmail.com>
2024-01-04 10:55:43 -0300

    created animatios js for imgs and text information

---
+++ js/select-plant-img.js
@@ -0,0 +1,31 @@
+const selectorList = document.querySelectorAll('.selector');
+
+let selectorArray = Array.from(selectorList);
+
+const imageBox = document.querySelector('.img__container')
+
+for (let i = 0; i < selectorList.length; i++) {
+
+    const selector = selectorList[i];
+
+    let deg = i * 90
+
+    function rotate() {
+
+        imageBox.animate(
+            [
+                { transform: `rotate(${deg}deg)`},
+            ],
+    
+            {
+                duration: 2000,
+                iterations: 1,
+                fill: 'forwards',
+                easing: 'ease-in-out'
+            },
+        )
+    }
+
+    selector.addEventListener('click', rotate)
+
+}
---
+++ js/select-plant-text.js
@@ -0,0 +1,44 @@
+const selectorList = document.querySelectorAll('.selector');
+
+const textList = document.querySelectorAll('.text__information');
+
+let selectorArray = Array.from(selectorList);
+
+let textArray = Array.from(textList);
+
+for (let i = 0; i < selectorList.length; i++) {
+
+    const selector = selectorList[i];
+
+    const text = document.querySelector(`.text__${selector.classList[1]}`);
+
+    function rotate() {
+
+        text.style.display = 'block'
+
+        text.animate(
+            [
+                { opacity: 1 },
+            ],
+    
+            {
+                duration: 2000,
+                iterations: 1,
+                fill: 'forwards',
+                easing: 'ease-in-out'
+            },
+        )
+
+        textArray.splice(i,1)
+
+        textArray.forEach((element) => {
+            element.style.opacity = 0;
+            element.style.display = 'none';
+        })
+
+        textArray = Array.from(textList);
+    }
+
+    selector.addEventListener('click', rotate)
+
+}
